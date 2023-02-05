abc070169e70fa17941db8531698db730907b9d7
Ranis1612 <Ranis1612@mail.ru>
2023-02-05 20:12:17 +0500

    Задачи по 2му семинару

---
+++ Sem3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4 // 16, 4 -> кратно
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+int num = (num2 / num1);
+int ost = num2 % num1;
+if(num2 == num * num1)
+{
+  Console.WriteLine($"{num2} кратно {num1}");
+}
+else if(num1 > num2)
+{
+  Console.WriteLine("Задача не имеет решения т.к первое число больше второго");
+  return;
+}
+else
+{
+  Console.WriteLine($"{num2} не кратно {num1}");
+  Console.WriteLine($" остаток {ost}");
+};
---
+++ Sem5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.  5, 25 -> да  //  4, 16 -> да //  25, 5 -> да  //  8,9 -> нет
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = num1 * num1;
+int num4 = num2 * num2;
+if(num1 == num4 || num2 == num3)
+{
+  Console.Write("Да ");
+}
+else
+{
+  Console.Write("Нет ");
+}
